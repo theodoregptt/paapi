c774f47f02bd84dbb4e28d9a3d170406a9b113ec
mehmettberke <dxd3d9@gmail.com>
2024-05-24 16:06:33 +0300

    w

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-console.log("hello")
+console.log("Server is starting...");
+
 var ProductAdvertisingAPIv1 = require("./src/index");
-
 var defaultClient = ProductAdvertisingAPIv1.ApiClient.instance;
 
 defaultClient.accessKey = "AKIAJNYDUIUL3RXQ5F3Q";
