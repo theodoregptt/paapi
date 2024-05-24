e3f4529d2bbd8852fd0030f1feeb6e8a0ef7238a
mehmettberke <dxd3d9@gmail.com>
2024-05-24 16:37:08 +0300

    cors update

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 const port = process.env.PORT || 3000;
 
 console.log("Server is starting...");
@@ -37,16 +39,7 @@ app.get("/search", (req, res) => {
     }
   );
 });
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-  );
-  next();
-});
+
 app.listen(port, () => {
   console.log(`API server running at http://localhost:${port}`);
 });
